06a9e13f33057128590d62ba473dbc4764c21b9a
joshuaja <joshuaja@google.com>
2020-07-14 19:23:15 -0400

    Refactored Vungle Android adapter to separate Banner implementation
    PiperOrigin-RevId: 321257571

--- ThirdPartyAdapters/vungle/vungle/src/main/java/com/google/ads/mediation/vungle/VungleMediationAdapter.java
+++ ThirdPartyAdapters/vungle/vungle/src/main/java/com/google/ads/mediation/vungle/VungleMediationAdapter.java
@@ -186,7 +186,7 @@ public class VungleMediationAdapter extends Adapter
     VungleInitializer.getInstance()
         .initialize(
             appID,
-            context.getApplicationContext(),
+            mediationRewardedAdConfiguration.getContext(),
             new VungleInitializationListener() {
               @Override
               public void onInitializeSuccess() {
